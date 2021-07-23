ec8ac20a1328ad9e908f7330a97df8520e8b655f
adrianensis <adrian.egea.comenge@gmail.com>
2021-07-23 19:38:50 +0200

    Don't regenerate code if no changes

--- scripts/generateClassList.py
+++ scripts/generateClassList.py
@@ -1,10 +1,11 @@
 import os  
 import re
 import shutil
+import filecmp
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 cwd = cwd + "/.."
-print(cwd) 
+#print(cwd) 
 
 os.chdir(cwd)
 
@@ -22,7 +23,7 @@ for folder in folders:
             file_name = os.path.join(root, f)
             if ".hpp" in file_name and "Macros" not in file_name:
                 with open(file_name, 'r') as file:
-                    print("looking for CLASS " + file_name)
+                    #print("looking for CLASS " + file_name)
                     lines = file.readlines()
                     for line in lines:
                         match = re.search(r'^\s*CLASS\s*\(\s*(\w+)\s*,\s*\w+\s*\)\s*', line)
@@ -35,46 +36,45 @@ for folder in folders:
                             if class_name not in class_list:
                                 class_list.append(class_name)
 
-print(class_list)
-print(includes)
+# print(class_list)
+# print(includes)
 
 generated_code_dirname = "generated-code"
+generated_code_dirname_tmp = os.path.join(generated_code_dirname, "tmp")
 generated_code_path = os.path.join(cwd, generated_code_dirname)
+generated_code_path_tmp = os.path.join(generated_code_path, "tmp")
+class_manager_generated = "ClassManager.generated"
+class_manager_includes_generated = "ClassManager.includes.generated"
+class_manager_generated_file_path = os.path.join(generated_code_path, class_manager_generated)
+class_manager_generated_file_path_tmp = os.path.join(generated_code_path_tmp, class_manager_generated)
+class_manager_includes_generated_file_path = os.path.join(generated_code_path, class_manager_includes_generated)
+class_manager_includes_generated_file_path_tmp = os.path.join(generated_code_path_tmp, class_manager_includes_generated)
 
 if os.path.isdir(generated_code_path):
-    shutil.rmtree(generated_code_dirname)
+    if os.path.isdir(generated_code_path_tmp):
+        shutil.rmtree(generated_code_dirname_tmp)
+else:
+    os.mkdir(generated_code_dirname)
 
-os.mkdir(generated_code_dirname)
+os.mkdir(generated_code_dirname_tmp)
 
-with open(os.path.join(generated_code_path, "ClassManager.generated"), "w") as class_list_file:
+with open(class_manager_generated_file_path_tmp, "w") as file:
     for class_name in class_list:
-        class_list_file.write("REGISTER_CLASS_BY_NAME("+class_name+")\n")
+        file.write("REGISTER_CLASS_BY_NAME("+class_name+")\n")
 
-with open(os.path.join(generated_code_path, "ClassManager.includes.generated"), "w") as includes_list_file:
+with open(class_manager_includes_generated_file_path_tmp, "w") as file:
     for include_path in includes:
-        includes_list_file.write("#include \""+include_path+"\"\n")
+        file.write("#include \""+include_path+"\"\n")
 
-# for root,d_names,f_names in os.walk(path):
-#     for f in f_names:
-#         file_name = os.path.join(root, f)
-#         if ".hpp" in file_name:
-#             with open(file_name, 'r') as file:
-#                 print("looking for includes " + file_name)
-#                 data = file.read()
 
-#                 includes = []
-#                 forwards = []
+overwrite = True
+if os.path.isfile(class_manager_generated_file_path) and os.path.isfile(class_manager_includes_generated_file_path):
+    if filecmp.cmp(class_manager_generated_file_path, class_manager_generated_file_path_tmp, shallow=True) and filecmp.cmp(class_manager_includes_generated_file_path, class_manager_includes_generated_file_path_tmp, shallow=True):
+        print(class_manager_generated + " and " + class_manager_includes_generated_file_path + " have no changes!")
+        overwrite = False
 
-#                 for c in class_list:
-#                     matchClass = re.search(r''+ c , data) # + '^(\s*\*)'
-#                     if matchClass:
-#                         print(matchClass.group())
-#                         forwards.append(c)
+if overwrite:
+    print(class_manager_generated + " and " + class_manager_includes_generated_file_path + " have changes!")
 
-#                     # matchPointer = re.search(r''+ c + '\s*\*', data)
-#                     # if matchPointer:
-#                     #     print(matchPointer.group())
-#                     #     forwards.append(c)
-
-#                 print("includes: " + str(includes))
-#                 print("forwards: " + str(forwards))
+    shutil.move(class_manager_generated_file_path_tmp, class_manager_generated_file_path)
+    shutil.move(class_manager_includes_generated_file_path_tmp, class_manager_includes_generated_file_path)
