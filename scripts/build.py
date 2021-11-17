9af892194fabd9aa42715a81651487a48deca853
adrianensis <adrianensis@gmail.com>
2021-11-17 22:58:32 +0100

    CMake now working on Windows

--- scripts/build.py
+++ scripts/build.py
@@ -1,13 +1,14 @@
 import sys
 import os
 import getopt
+import platform
 
 import generateCode
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 cwd = cwd + "/.."
 os.chdir(cwd)
-#print(cwd) 
+print(cwd) 
   
 buildDir="build"
 buildType="Debug"
@@ -44,8 +45,32 @@ if not os.path.isdir(buildDir):
 
 os.chdir(buildDir)
 
+system_name = platform.system()
+system_info = str(platform.uname())
+
+print(system_name)
+print(system_info)
+
+cmake_generator = ""
+if system_name == "Linux" or system_name == "Linux2":
+    # linux
+    cmake_generator = '-G "Unix Makefiles"'
+elif system_name == "Darwin":
+    # OS X
+    pass
+elif system_name == "Windows":
+    # Windows...
+    cmake_generator = '-G "Visual Studio 17 2022"'
+# elif ANDROID:
+#     # Windows...
+#     pass
+# elif IOS:
+#     # Windows...
+#     pass
+
 # -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-buildCommand = 'cmake -DCMAKE_BUILD_TYPE={buildType} -DBUILD_UNIT_TESTS={buildUnitTests} -DBUILD_INTEGRATION_TESTS={buildIntegrationTests} -DBUILD_TOOLS={buildTools} -DENABLE_LOGS={enableLogs} ..'.format(
+buildCommand = 'cmake {cmake_generator} -DCMAKE_BUILD_TYPE={buildType} -DBUILD_UNIT_TESTS={buildUnitTests} -DBUILD_INTEGRATION_TESTS={buildIntegrationTests} -DBUILD_TOOLS={buildTools} -DENABLE_LOGS={enableLogs} ..'.format(
+  cmake_generator = cmake_generator,
   buildType = buildType,
   buildUnitTests = buildUnitTests,
   buildIntegrationTests = buildIntegrationTests,
@@ -54,4 +79,19 @@ buildCommand = 'cmake -DCMAKE_BUILD_TYPE={buildType} -DBUILD_UNIT_TESTS={buildUn
 )
 os.system(buildCommand)
 
-os.system('make -j8')
+if system_name == "Linux" or system_name == "Linux2":
+    # linux
+    os.system('make -j8')
+elif system_name == "Darwin":
+    # OS X
+    pass
+elif system_name == "Windows":
+    # Windows...
+    # Requires msbuild added to the PATH
+    os.system('msbuild druid.sln')
+# elif ANDROID:
+#     # Windows...
+#     pass
+# elif IOS:
+#     # Windows...
+#     pass
