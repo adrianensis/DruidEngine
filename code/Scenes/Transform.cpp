2e696f9797006bcddccaa7bf34c610d4ce865300
adrianensis <adrianensis@gmail.com>
2021-12-09 23:31:04 +0100

    Refactor Transform::getWorldPosition

--- code/Scenes/Transform.cpp
+++ code/Scenes/Transform.cpp
@@ -57,12 +57,7 @@ const Vector3 &Transform::getWorldPosition() const
 
 	if (mParent)
 	{
-		mWorldPosition.add(mParent->getWorldPosition());
-
-		Matrix4 rotationMatrix;
-		rotationMatrix.init(mParent->getRotationMatrix());
-
-		mWorldPosition = Vector3(rotationMatrix.mulVector(Vector4(mWorldPosition, 1.0f)));
+		mWorldPosition = mParent->getModelMatrix().mulVector(Vector4(mWorldPosition, 1));
 	}
 
 	return mWorldPosition;
